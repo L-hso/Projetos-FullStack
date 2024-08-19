3c091bc0ee6644a80f6e5f0265c07fb3c9bcfd7e
Luis Henrique <119861034+L-hso@users.noreply.github.com>
2024-08-19 20:30:40 +0000

    vmétodopost parcialmente implementado

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import { v4 as uuid } from "uuid";
 
 const server = fastify();
 
-
+const posts = [];
 
 await server.register(cors, {
   credentials: true,
@@ -12,15 +12,16 @@ await server.register(cors, {
 })
 
 server.get("/posts", (request, reply) => {
-  return [{
-    id: uuid(),
-    profilePicture: "https://i.pinimg.com/originals/cb/2c/15/cb2c15f4607465c0d92a92133e44382b.jpg",
-    name: "Luis",
-    address: "@L-hso",
-    text: "Mussum Ipsum, cacilds vidis litro abertis. Si num tem leite então bota uma pinga aí cumpadi! Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Casamentiss faiz malandris se pirulitá. Per aumento de cachacis, eu reclamis.",
-    likes: 123,
-    comments: 100
-  }];
+  return reply.status(200).send(posts);
 });
+
+server.post("/posts", (request, reply) =>{
+  let id = uuid()
+
+
+  posts.push({id, ...request.body});
+
+  return reply.status(201).send();
+})
 
 server.listen({ port: process.env.PORT ?? 3333 });
