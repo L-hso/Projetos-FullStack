00fff043622d4eee4aee3880285864ebc585d85b
l-hso <lhsoliveira2008@hotmail.com>
2024-08-19 17:03:06 -0300

    Api alterada

--- api/index.js
+++ api/index.js
@@ -8,11 +8,11 @@ const server = fastify();
 
 await server.register(cors, {
   credentials: true,
-  origin: false
+  origin: "https://symmetrical-space-adventure-r9rj7pxj5jghwq54-5173.app.github.dev"
 })
 
 server.get("/posts", (request, reply) => {
-  return {
+  return [{
     id: uuid(),
     profilePicture: "https://i.pinimg.com/originals/cb/2c/15/cb2c15f4607465c0d92a92133e44382b.jpg",
     name: "Luis",
@@ -20,7 +20,7 @@ server.get("/posts", (request, reply) => {
     text: "Mussum Ipsum, cacilds vidis litro abertis. Si num tem leite então bota uma pinga aí cumpadi! Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Casamentiss faiz malandris se pirulitá. Per aumento de cachacis, eu reclamis.",
     likes: 123,
     comments: 100
-  };
+  }];
 });
 
 server.listen({ port: process.env.PORT ?? 3333 });
